2f451bb053684d7150f43f98af63f0c240189301
Iván <54847509+ivansanmartin@users.noreply.github.com>
2022-08-21 23:38:44 -0400

    Update BinaryDecimalBinary program
    I have added a new package (os) for clear the console when changed process from one another
    
    (Functional only in Windows, soon added in Linux operative system)

--- BinaryDecimalBinary.py
+++ BinaryDecimalBinary.py
@@ -1,4 +1,7 @@
-def binarioDecimal(numeroBinario):
+from os import system
+
+
+def binario_decimal(numeroBinario):
     potencias_de_dos = (128, 64, 32, 16, 8, 4, 2, 1)
     binarios = []
     suma = 0
@@ -12,7 +15,7 @@ def binarioDecimal(numeroBinario):
     return suma
 
 
-def decimalBinario(numeroDecimal):
+def decimalB_binario(numeroDecimal):
     potencias_de_dos = (128, 64, 32, 16, 8, 4, 2, 1)
     binario = []
     decimal_nuevo = numeroDecimal
@@ -24,43 +27,46 @@ def decimalBinario(numeroDecimal):
             binario.append(0)
     return "".join(map(str, binario))
 
+def salir():
+    print("Programa finalizado.")
+
 
 def inicio():
-    lista = ["1", "2"]
+    lista = ["1", "2", "3"]
     print('''
-
     Elija una opcion.
-
     \t [1] Pasar de decimal a binario
     \t [2] Pasar de binario a decimal
-    \n
+    \t [3] Salir
     ''')
 
     numero = str(input("Ingrese la opcion (1 o 2): "))
+    system("cls")
 
     while numero not in lista:
         print("ERROR! Ingresa un opcion correcta \n")
-        numero = str(input("Ingrese la opcion (1 o 2): "))
+        numero = str(input("Ingrese la opcion (1, 2 o 3): "))
 
     if numero == "1":
         decimal = int(input("Ingrese un numero decimal que desea convertir: "))
-        resultado = decimalBinario(decimal)
+        resultado = decimalB_binario(decimal)
+        system("cls")
         print(f"""
         El numero {decimal} ha sido transformado correctamente. 
-
         Numero decimal a binario: {resultado}
         """)
 
     elif numero == "2":
         binario = str(input("Ingrese el numero binario que desea convertir: "))
-        resultado = binarioDecimal(binario)
+        resultado = binario_decimal(binario)
+        system("cls")
         print(f"""
         El numero {binario} ha sido transformado correctamente
-
-
         Numero binario a decimal: {resultado}
         """)
-
+    elif numero == "3":
+        salir()
 
 
 inicio()
+
